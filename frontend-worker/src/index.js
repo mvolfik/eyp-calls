1429ab14e50b364142b92cdf31f4f289177682c2
Matěj Volf <mat.volfik@gmail.com>
2021-01-08 19:22:28 +0100

    Fix: JS Date() months are 0-indexed

--- frontend-worker/src/index.js
+++ frontend-worker/src/index.js
@@ -18,7 +18,7 @@ let calls;
 
 
 function formatDate(d) {
-  return `${d.getFullYear().toString().padStart(4, "0")}-${d.getMonth().toString().padStart(2, "0")}-${d.getDate().toString().padStart(2, "0")}`;
+  return `${d.getFullYear().toString().padStart(4, "0")}-${(d.getMonth() + 1).toString().padStart(2, "0")}-${d.getDate().toString().padStart(2, "0")}`;
 }
 
 function formatTime(d) {
