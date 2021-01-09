cce3a5310f48095d73fd263c103d756f404ac844
Matěj Volf <mat.volfik@gmail.com>
2021-01-09 23:12:42 +0100

    Allow formatting for seconds

--- frontend-worker/src/index.js
+++ frontend-worker/src/index.js
@@ -21,8 +21,12 @@ function formatDate(d) {
   return `${d.getFullYear().toString().padStart(4, "0")}-${(d.getMonth() + 1).toString().padStart(2, "0")}-${d.getDate().toString().padStart(2, "0")}`;
 }
 
-function formatTime(d) {
-  return `${d.getHours().toString().padStart(2, "0")}:${d.getMinutes().toString().padStart(2, "0")}`;
+function formatTime(d, seconds) {
+  let output = `${d.getHours().toString().padStart(2, "0")}:${d.getMinutes().toString().padStart(2, "0")}`;
+  if (seconds) {
+    output += `:${d.getSeconds().toString().padStart(2, "0")}`;
+  }
+  return output;
 }
 
 function formatDateTime(d) {
